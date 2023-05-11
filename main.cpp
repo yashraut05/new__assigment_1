d2c606a1af0ee265ffb7bd2e63d378666fb29473
Yash Raut 05 <131888738+yashraut05@users.noreply.github.com>
2023-05-11 20:39:15 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "NAME: HELLO MY NAME IS YASH ASHOK RAUT" << endl;
+    cout <<"ADDRESS: NEW NANDANWAN NAGPUR" << endl;
+    return 0;
+}
